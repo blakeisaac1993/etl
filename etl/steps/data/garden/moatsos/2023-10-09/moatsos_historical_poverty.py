c251444d516c8268b4b6be0e43602ff330da7055
Pablo Arriagada <paarriagadap@gmail.com>
2023-12-29 17:36:37 +0000

    :sparkles: add smoothed columns for all data

--- etl/steps/data/garden/moatsos/2023-10-09/moatsos_historical_poverty.py
+++ etl/steps/data/garden/moatsos/2023-10-09/moatsos_historical_poverty.py
@@ -9,6 +9,7 @@ from etl.helpers import PathFinder, create_dataset
 # Get paths and naming conventions for current step.
 paths = PathFinder(__file__)
 
+# Define poverty lines (dollar a day)
 poverty_lines = [
     "190",
     "500",
@@ -16,6 +17,10 @@ poverty_lines = [
     "3000",
 ]
 
+# Define suffixes for smoothed variables
+smooth_suffix = ["", "_smooth"]
+
+# Define first and last year of the dataset
 FIRST_YEAR = 1820
 LAST_YEAR = 2018
 
@@ -37,7 +42,16 @@ def run(dest_dir: str) -> None:
     tb = smooth_estimates(tb, poverty_lines)
 
     # Create headcount, above and between poverty lines variables
-    tb = create_above_and_between_vars(tb, poverty_lines)
+    tb = create_above_and_between_vars(tb, poverty_lines, smooth_suffix)
+
+    tb = geo.harmonize_countries(
+        df=tb,
+        countries_file=paths.country_mapping_path,
+    )
+    tb = tb.set_index(["country", "year"], verify_integrity=True)
+
+    # Add metadata by code
+    tb = add_metadata_vars(tb)
 
     #
     # Save outputs.
@@ -98,66 +112,62 @@ def smooth_estimates(tb: Table, poverty_lines: list) -> Table:
     return tb
 
 
-def create_above_and_between_vars(tb: Table, poverty_lines: list) -> Table:
+def create_above_and_between_vars(tb: Table, poverty_lines: list, smooth_suffix: list) -> Table:
     """
     Create additional variables from the share and number in poverty: above and between poverty lines (or CBN).
     Also, add metadata to these variables.
     """
     # Add population column to estimate other variables
     tb["pop"] = tb["headcount_cbn"] / (tb["headcount_ratio_cbn"] / 100)
+    tb["pop"] = tb["pop"].astype(float)
+
+    # Create headcount_cbn_smooth
+    tb["headcount_cbn_smooth"] = tb["headcount_ratio_cbn_smooth"] * tb["pop"] / 100
 
     # Create additional variables
     # Define columns to use
 
-    cols_number = ["headcount_cbn"]
+    cols_number = ["headcount_cbn", "headcount_cbn_smooth"]
     cols_above = []
     cols_number_above = []
     cols_between = []
     cols_number_between = []
 
-    for povline in poverty_lines:
-        tb[f"headcount_{povline}"] = tb[f"headcount_ratio_{povline}"] * tb["pop"] / 100
-        cols_number.append(f"headcount_{povline}")
+    for smooth in smooth_suffix:
+        for povline in poverty_lines:
+            tb[f"headcount_{povline}{smooth}"] = tb[f"headcount_ratio_{povline}{smooth}"] * tb["pop"] / 100
+            cols_number.append(f"headcount_{povline}{smooth}")
 
-    # Share and number of people above poverty lines
-    for povline in poverty_lines:
-        tb[f"headcount_ratio_above_{povline}"] = 100 - tb[f"headcount_ratio_{povline}"]
-        cols_above.append(f"headcount_ratio_above_{povline}")
-        tb[f"headcount_above_{povline}"] = tb[f"headcount_ratio_above_{povline}"] * tb["pop"] / 100
-        cols_number_above.append(f"headcount_above_{povline}")
+        # Share and number of people above poverty lines
+        for povline in poverty_lines:
+            tb[f"headcount_ratio_above_{povline}{smooth}"] = 100 - tb[f"headcount_ratio_{povline}{smooth}"]
+            cols_above.append(f"headcount_ratio_above_{povline}{smooth}")
+            tb[f"headcount_above_{povline}{smooth}"] = tb[f"headcount_ratio_above_{povline}{smooth}"] * tb["pop"] / 100
+            cols_number_above.append(f"headcount_above_{povline}{smooth}")
 
-    # Also do it for cbn
-    tb["headcount_ratio_above_cbn"] = 100 - tb["headcount_ratio_cbn"]
-    cols_above.append("headcount_ratio_above_cbn")
-    tb["headcount_above_cbn"] = tb["headcount_ratio_above_cbn"] * tb["pop"] / 100
-    cols_number_above.append("headcount_above_cbn")
+        # Also do it for cbn
+        tb[f"headcount_ratio_above_cbn{smooth}"] = 100 - tb[f"headcount_ratio_cbn{smooth}"]
+        cols_above.append(f"headcount_ratio_above_cbn{smooth}")
+        tb[f"headcount_above_cbn{smooth}"] = tb[f"headcount_ratio_above_cbn{smooth}"] * tb["pop"] / 100
+        cols_number_above.append(f"headcount_above_cbn{smooth}")
 
-    # Share and number of people in between poverty lines (World Bank)
-    # For each poverty line in cols_wb
-    for i in range(len(poverty_lines)):
-        if i != 0:
-            tb[f"headcount_ratio_between_{poverty_lines[i-1]}_{poverty_lines[i]}"] = (
-                tb[f"headcount_ratio_{poverty_lines[i]}"] - tb[f"headcount_ratio_{poverty_lines[i-1]}"]
-            )
-            cols_between.append(f"headcount_ratio_between_{poverty_lines[i-1]}_{poverty_lines[i]}")
+        # Share and number of people in between poverty lines (World Bank)
+        # For each poverty line in cols_wb
+        for i in range(len(poverty_lines)):
+            if i != 0:
+                tb[f"headcount_ratio_between_{poverty_lines[i-1]}_{poverty_lines[i]}{smooth}"] = (
+                    tb[f"headcount_ratio_{poverty_lines[i]}"] - tb[f"headcount_ratio_{poverty_lines[i-1]}{smooth}"]
+                )
+                cols_between.append(f"headcount_ratio_between_{poverty_lines[i-1]}_{poverty_lines[i]}{smooth}")
 
-            tb[f"headcount_between_{poverty_lines[i-1]}_{poverty_lines[i]}"] = (
-                tb[f"headcount_ratio_between_{poverty_lines[i-1]}_{poverty_lines[i]}"] * tb["pop"] / 100
-            )
-            cols_number_between.append(f"headcount_between_{poverty_lines[i-1]}_{poverty_lines[i]}")
+                tb[f"headcount_between_{poverty_lines[i-1]}_{poverty_lines[i]}{smooth}"] = (
+                    tb[f"headcount_ratio_between_{poverty_lines[i-1]}_{poverty_lines[i]}{smooth}"] * tb["pop"] / 100
+                )
+                cols_number_between.append(f"headcount_between_{poverty_lines[i-1]}_{poverty_lines[i]}{smooth}")
 
     # Round to integer numbers
     tb[cols_number + cols_number_above + cols_number_between] = tb[
         cols_number + cols_number_above + cols_number_between
     ].round()
-
-    tb = geo.harmonize_countries(
-        df=tb,
-        countries_file=paths.country_mapping_path,
-    )
-    tb = tb.set_index(["country", "year"], verify_integrity=True)
-
-    # Add metadata by code
-    tb = add_metadata_vars(tb)
 
     return tb
